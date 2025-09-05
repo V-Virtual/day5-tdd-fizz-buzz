35451afc0f660b585565c68a72119e1ce2b481af
Gavin Zhang <ZHANGGA7@OOCL.COM>
2025-09-05 11:44:47 +0800

    feat: add test and countOff for normal number

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,10 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public String countOff(int number) {
+        if(number % 3 != 0 && number % 5 != 0) {
+            return String.valueOf(number);
+        }
+        return null;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_normal_number_when_input_normal_number() {
+        //Given
+        int number = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //When
+        String result = fizzBuzz.countOff(number);
+        //Then
+        assertEquals("1", result);
+    }
 }
